7a5f847a2aaad6b688919c2472b239a00c1ec444
Lhorrany Cerqueira <lhorranycerqueira@gmail.com>
2026-03-09 09:23:47 -0300

    Adiciona conteúdo ao README e cria a classe Main com método principal adiciona os arquivos .java

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Desenvolvimento de Software I
 Aulas de Desenvolvimento de Software I com o professor João Siles utilizando Java.
+como configura o java no computaador, as regras e explicando um pouquinho como o codigo funciona.
+stackedit
---
+++ aula01/Main.java
@@ -0,0 +1,11 @@
+package aula01;
+
+public class Main { //o nome do arquivo deve ser o mesmo nome da classe principal TEM QUE SER COM LETRA MAIUSCULA
+    // a classe principal vai abracar tudo do projeto
+  public static void main(String[] args) { //recebendo parametros
+    System.out.println("Hello World"); // ação, de saida
+  }
+}
+
+//todo comando e ação tem que terminar com ;
+
