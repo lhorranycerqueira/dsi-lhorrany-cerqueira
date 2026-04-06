4855a76f0b2f7fa3ad7d383a36c6a9e356d9709f
Lhorrany Cerqueira <lhorranycerqueira@gmail.com>
2026-04-06 08:49:52 -0300

    exercicios

---
+++ aula3/Variaveis.java
@@ -0,0 +1,10 @@
+
+public class Variaveis {
+    public static void main(String[] args) {
+        int myNum;
+        myNum = 15;
+        String msgTeste;
+        msgTeste = "Oie";
+        System.out.println(myNum + " " + msgTeste);
+    }
+}
