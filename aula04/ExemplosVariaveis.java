b79978353bf32a523b5218adde53fd853a67507b
Lhorrany Cerqueira <lhorranycerqueira@gmail.com>
2026-04-13 09:19:51 -0300

    exercicio de tipo de variaveis

---
+++ aula04/ExemplosVariaveis.java
@@ -0,0 +1,45 @@
+public class ExemplosVariaveis {
+    public static void main(String[] args) {
+        int n1, n2, n3;
+        n1 = n2 = 10;
+        n3 = 5;
+        System.out.println(n1 + n2 + n3);
+
+
+
+        // boooooooooooleano
+        boolean isRodrigoGay = true;
+        boolean isRodrigoHetero = false;
+        
+        System.out.println(isRodrigoGay);
+        System.out.println(isRodrigoHetero);
+
+        // string
+
+        String txt = "spotifyrson";
+        System.out.println("o que eu gosto: " + txt );
+
+        //long
+
+        long meuNumero = 2000000000L;
+        System.out.println(meuNumero);
+
+
+
+        // double e float
+        double double1 = 29E4d; // esse eh pra indicar potencia de 10 pra isso serve o E
+        double doubleSemPotencia = 45d;
+        float meuNumeroFloat = 54.2f;
+        System.out.println(double1);
+        System.out.println(doubleSemPotencia);
+        System.out.println(meuNumeroFloat);
+
+
+        //byte short e int
+        byte meuByte = 12;
+        short myShortKing = 9000;
+        int numeroInteirissimo = 432525453;
+        System.out.println(meuByte + myShortKing + numeroInteirissimo);
+    }
+
+}
